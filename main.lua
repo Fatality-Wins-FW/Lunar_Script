5241ccddafc118a13c2e3b2be1e9730165043c49
Fatality-Wins-FW <leckayri96@gmail.com>
2026-09-06 17:04:34 +0200

    ghgdfgdffdg

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local HttpService = game:GetService("HttpService")
 
 local supported_games = {
-    ["286090429"] = "https://raw.githubusercontent.com/your-username/your-repo/main/LunarArsenalV1.lua",
+    ["286090429"] = "https://github.com/Fatality-Wins-FW/Lunar_Script/raw/refs/heads/main/286090429.lua",
 }
 
 local function loadScript(url)
