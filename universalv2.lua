0bc8bd5aaf226a2b375bd5c6fa48e128991af447
Fatality-Wins-FW <leckayri96@gmail.com>
2026-09-18 13:39:06 +0200

    fdsfdsf

---
+++ universalv2.lua
@@ -0,0 +1,16 @@
+local UI = Instance.New("ScreenGui")
+local MainFrame = Instance.New("Frame")
+
+windowconfig = {
+    Title = "Lunar",
+    Subtitle = "Universal V2",
+    Footer = "Lunar's Script",
+    BackgroundColor = Color3.new(.114, 0.106, 0.106),
+}
+
+tabs = {
+    "Combat",
+    "Visuals",
+    "Misc",
+    "Settings",
+}
